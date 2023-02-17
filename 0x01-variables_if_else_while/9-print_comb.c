ac53a5a271c08f092ab352f0381fd44403ad9e4d
Damilare <ajosedamilare09@gmail.com>
2023-02-17 13:58:06 -0800

    Updated 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar( a + '0');
+		putchar(a + '0');
 		if (a < 9)
-		{		
-			putchar(',');
-			putchar(' ');
-		}
+	{		
+		putchar(',');
+		putchar(' ');
+	}
 	}	
 
 	putchar('\n');
